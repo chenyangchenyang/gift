2e0e3cb55fc2ff7efddb1ea7febd8c82c1375cf4
fjt61020 <757834415@qq.com>
2018-09-19 22:32:38 +0800

    Revert "Merge branch 'master' of https://github.com/chenyangchenyang/gift"
    This reverts commit 03e5571ec1dd838234804015f297eac0213bd9fd, reversing
    changes made to a5859ea7508aa50757c6fea01018b78cfe58670e.

--- miniGame/Assets/Scripts/PlayerControl.cs
+++ miniGame/Assets/Scripts/PlayerControl.cs
@@ -21,40 +21,15 @@ public class PlayerControl : MonoBehaviour
     private float startSpeed;
     private float lastSpeed;
 
-    public float SpeedForFirstSub;
-    public float SpeedForSecSub;
-
-    [HideInInspector]
-    public float CurStandSpeed;
-
-    [HideInInspector]
-    public float[] SpeedStand= new float[3];
-    public float[] SpeedAnimation = new float[3] { 1, 1, 1 };
-    public Color[] SpriteColor = new Color[3];
-
     void Start () 
 	{
         lastSpeed = speed;
-        lastPosition = gameObject.GetComponent<Rigidbody2D>().position;    
-
-        if(0 == GlobalTool.CurDeathCount)
-        {
-            CurStandSpeed = startSpeed;
-        }
-        SpeedStand[0] = startSpeed;
-        SpeedStand[1] = SpeedForFirstSub;
-        SpeedStand[2] = SpeedForSecSub;
-        GetComponent<Animator>().SetFloat("Speed", GlobalTool.animSpeed);  
-        for (int i = 0; i < transform.GetChild(0).childCount; ++i)
-        {
-            transform.GetChild(0).GetChild(i).gameObject.GetComponent<SpriteRenderer>().color = GlobalTool.spriteColor;
-        }
-    }
+        lastPosition = gameObject.GetComponent<Rigidbody2D>().position;
+	}
 	
 
 	void Update () 
 	{
-        print(GetComponent<Animator>().GetFloat("Speed"));
         speed += Time.deltaTime * addSpeed * Mathf.Log(startSpeed - speed + 2);
         if (speed < 0)
         {
@@ -72,7 +47,7 @@ public class PlayerControl : MonoBehaviour
             rb.sharedMaterial.friction = 1;
         }
 
-        UpdateSpeed();
+
     }
 
     private void FixedUpdate()
@@ -108,69 +83,5 @@ public class PlayerControl : MonoBehaviour
         }
 
         GameManager._instance.PlayFootStep();
-    }
-
-
-    private void UpdateSpeed()
-    {
-        //print("CurStandSpeed : "+ CurStandSpeed);
-
-        if(speed > CurStandSpeed)
-        {
-            speed = CurStandSpeed;
-        }
-    }
-
-
-    private float StepPerMS;
-    private float FinialV;
-    public void AdjustSpeed(float stepPerMS, float finalV)
-    {
-        StepPerMS = stepPerMS;
-        FinialV   = finalV;
-        StartCoroutine(OnAdjustSpeed());
-    }
-    private IEnumerator OnAdjustSpeed()
-    {
-        if(StepPerMS< 0)
-        {
-            while(speed > FinialV)
-            {
-                speed += StepPerMS;
-
-                yield return new WaitForSeconds(0.001f);
-            }
-        }
-        else
-        {
-            while (speed < FinialV)
-            {
-                speed += StepPerMS;
-
-                yield return new WaitForSeconds(0.001f);
-            }
-        }
-    }
-}
-
-public partial class GlobalTool
-{
-    public static int CurDeathCount = 0;
-    public static float animSpeed = 1;
-    public static Color spriteColor = Color.white;
-
-    public static void SetCurDeathCount(int idx)
-    {
-        PlayerControl player= GameManager._instance.Player.GetComponent<PlayerControl>();
-        
-        if(player.SpeedStand!= null && idx< player.SpeedStand.Length && idx>= 0)
-        {
-            player.CurStandSpeed = player.SpeedStand[idx];
-        }        
-        if (idx >= 0 && idx < player.SpeedAnimation.Length)
-        {
-            animSpeed = player.SpeedAnimation[idx];
-            spriteColor = player.SpriteColor[idx];
-        }
     }
 }
