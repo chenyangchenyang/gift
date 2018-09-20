aafecb8132b013710cb1e14134533600cdea0520
togchen <834851098@qq.com>
2018-09-21 04:08:13 +0800

    level5

---
+++ miniGame/Assets/Scripts/VideoChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoChange : MonoBehaviour
+{
+    public VideoPlayer video;
+
+    public void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+
+    public void Update()
+    {
+        if(video.frame>0  && !video.isPlaying)
+        {
+            string name = "Level1";
+
+            SceneManager.LoadScene(name);
+        }
+    }
+
+}
